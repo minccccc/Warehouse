3285c4e308b5fe50a19b8b9a31c957ccea4cca0e
Minko Minkov <Minko.Minkov@digitall.com>
2024-01-24 18:13:45 +0200

    Add UnitTests projects

--- src/Warehouse/Application/PipelineBehaviors/ValidationBehavior.cs
+++ src/Warehouse/Application/PipelineBehaviors/ValidationBehavior.cs
@@ -1,20 +1,17 @@
 ﻿using Application.Exceptions;
 using FluentValidation;
 using MediatR;
-using Microsoft.Extensions.Logging;
 
 namespace Application.PipelineBehaviors
 {
     public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
         where TRequest : IRequest<TResponse>
     {
-        private readonly ILogger<ValidationBehavior<TRequest, TResponse>> _logger;
         private readonly IEnumerable<IValidator<TRequest>> _validators;
 
-        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators, ILogger<ValidationBehavior<TRequest, TResponse>> logger)
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
         {
             _validators = validators;
-            _logger = logger;
         }
 
         public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
